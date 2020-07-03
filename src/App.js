17d3ff94c1aaa69e0b67ddd5932cf79604a445f4
Symbol <Symbolistic@live.com>
2020-07-02 21:11:25 -0400

    first commit, sending project to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [streamerData, setStreamerData] = useState([]);
+
+  useEffect( () => {
+    ( async () => {
+      const url = "https://wind-bow.glitch.me/twitch-api/streams/"
+      const streamerList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  
+      const data = await Promise.all(
+        streamerList.map(value => 
+          fetch(url + value).then(response => response.json())
+        )
+      );
+  
+      setStreamerData(data);
+      console.log(streamerData);
+    })()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">Twitch Streamers</h1>
+      <div id="streamers">
+      {(streamerData.length > 0) ? (
+              streamerData.map((val) => {
+                console.log(val)
+                if (val.stream === null) {
+                  const channelName = val._links.channel.split('/')
+                  return (
+                    <a key={channelName+1} className="results" href={`https://www.twitch.tv/${channelName[5]}`} target="_blank" rel="noopener noreferrer"  >
+                      <div>
+                      <h2 className="streamerName">{channelName[5]}</h2>
+                      <p className="streamerStatus">Status: <strong style={{color: "#53535f"}}>Offline</strong></p>
+                    </div></a>
+                  );
+                } else {
+                  const channelName = val._links.channel.split('/')
+                  console.log(`https://www.twitch.tv/${channelName[5]}`)
+                  return (
+                    <a key={channelName+1} className="results" href={`https://www.twitch.tv/${channelName[5]}`} target="_blank" rel="noopener noreferrer"  >
+                      <div>
+                        <h2 className="streamerName">{val.stream.channel.display_name}</h2>
+                        <p>Status: <strong style={{color: "green"}}>Online</strong></p>
+                        <p className="streamerStatus">Playing: {val.stream.channel.game}</p>
+                    </div></a>
+                  );
+                }
+              })
+            ) : ('')}
+      </div>
     </div>
   );
 }
