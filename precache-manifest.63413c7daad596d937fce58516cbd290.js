7777831f7731940a70c2fe4a0cb0e986308a5647
Symbol <Symbolistic@live.com>
2020-07-02 21:13:04 -0400

    Updates

---
+++ precache-manifest.63413c7daad596d937fce58516cbd290.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a117d15c6174db9d692ec2979b7a1102",
+    "url": "/react-twitch-api/index.html"
+  },
+  {
+    "revision": "5caf1432efe0e56b11e6",
+    "url": "/react-twitch-api/static/css/main.739ada2c.chunk.css"
+  },
+  {
+    "revision": "457d92ce83dd9d62cae3",
+    "url": "/react-twitch-api/static/js/2.d9504a60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-twitch-api/static/js/2.d9504a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5caf1432efe0e56b11e6",
+    "url": "/react-twitch-api/static/js/main.badff4ce.chunk.js"
+  },
+  {
+    "revision": "d2de0635aec969f6f551",
+    "url": "/react-twitch-api/static/js/runtime-main.16f0a2e4.js"
+  },
+  {
+    "revision": "83624d6d78b5111fdae7db6d3cd1e145",
+    "url": "/react-twitch-api/static/media/background.83624d6d.png"
+  }
+]);
